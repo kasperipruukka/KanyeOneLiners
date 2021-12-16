2fdf7d9a7eaacabae2e7d6a2f910c044edec74d0
Kasperi Pruukka <kasperi.pruukka@gmail.com>
2021-12-16 19:50:26 +0200

    Refaktoroidaan useQuotes-hookkia.
    Poistetaan testifunktio.
    Korjataan tilaa koskeva muuttuja useState -> setState.

--- src/useQuotes.js
+++ src/useQuotes.js
@@ -2,14 +2,10 @@ import { useContext, useEffect } from "react";
 import { AppContext } from "./app-context";
 
 const useQuotes = () => {
-    const [state, useState] = useContext(AppContext);
 
-    const testiFunkkari = () => {
-        console.log('Testataan AppContext ja useContext toimivuus');
     };
 
     return {
-        testiFunkkari,
     };
 }
 
