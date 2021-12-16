b9f8c8b11c5f9d928a49a7a8153b36cb23ac6474
Kasperi Pruukka <kasperi.pruukka@gmail.com>
2021-12-16 19:46:54 +0200

    QuoteFetch komponenttiin toiminnallisuutta.
    Tuodaan komponentille käyttöön useQuotes-hook. Lisätään fetchQuote-painikkeelle onClick-attribuutti. Painiketta klikattaessa laukeaa loadQuote-metodi.

--- src/components/QuoteFetch.js
+++ src/components/QuoteFetch.js
@@ -1,7 +1,11 @@
+import useQuotes from "../useQuotes";
+
 const QuoteFetch = () => {
+    const {loadQuote} = useQuotes();
+
     return(
         <div className="w-full">
-            <button className="m-2 btn btn-primary w-3/4 btnMaxWidth">Fetch 1 quote</button>
+            <button id="fetchQuote" onClick={loadQuote} className="m-2 btn btn-primary w-3/4 btnMaxWidth">Fetch 1 quote</button>
         </div>
     );
 }
