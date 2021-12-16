8589cbfc416956c932b4ba53e6f09a19b246e71d
Kasperi Pruukka <kasperi.pruukka@gmail.com>
2021-12-16 19:55:44 +0200

    Metodien lisäystä hookkiin.
    Luodaan asynkroninen loadQuote-metodi, joka käy hakemassa APIsta yksittäisen quoten.
    
    Quote passataan saveQuote-metodille, jonka tehtävänä on asettaa haettu quote stateen ja muuttaa sen tilaa samalla.
    
    Lopuksi hook palauttaa kaksi metodia ja quotes-propertyn.

--- src/useQuotes.js
+++ src/useQuotes.js
@@ -2,10 +2,26 @@ import { useContext, useEffect } from "react";
 import { AppContext } from "./app-context";
 
 const useQuotes = () => {
+    const [state, setState] = useContext(AppContext);
 
+    const loadQuote = async () => {
+        const API_URL = 'https://api.kanye.rest/'
+        let response = await fetch(API_URL);
+        let data = await response.json();
+        saveQuote(data.quote);
     };
 
+    const saveQuote = (quote) => {
+        let allQuotes = [...state.quotes, quote];
+        setState({...state, quotes: allQuotes});
+    };
+
+
+
     return {
+        loadQuote,
+        saveQuote,
+        quotes: state.quotes,
     };
 }
 
