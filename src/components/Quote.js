6c23d1801f485fef8d78e43d0ea33d903bf29351
Kasperi Pruukka <kasperi.pruukka@gmail.com>
2021-12-16 22:08:25 +0200

    Lisätään ehtolause QuoteListille.
    Mikäli ei löydy yhtään quotea ->
    Näytetään "No quotes!"-merkkijono.
    
    Tyylittelyä Quote-komponentille.

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,7 +1,17 @@
+import useQuotes from "../useQuotes";
+
 const Quote = ({quote}) => {
+    //const {deleteQuote} = useQuotes();
+
+    // const deleteClick = () => {
+    //     deleteQuote();
+    // }
+
     return(
-        <div>
-            <p>{quote}</p>
+        <div className="singleQuote m-3 p-1 jumbotron jumbotron-fluid bg-gradient-to-r from-white via-sky-100 to-white hover:cursor-pointer">
+            <div className="container flex justify-center">
+                <p onClick={deleteClick} className="text-center w-3/5 text-lg"><i>{quote}</i></p>
+            </div>
         </div>
     );
 }
--- src/components/QuoteList.js
+++ src/components/QuoteList.js
@@ -3,19 +3,31 @@ import Quote from "./Quote";
 
 const QuoteList = () => {
     const {quotes} = useQuotes();
-    return(
-        <div className="quoteWidth">
-            <h4>Quotes</h4>
-            <hr />
-            {
-                quotes.map((quote, index) => {
-                    return (
-                        <Quote key={quote + '-' + index} quote={quote} />
-                    );
-                })
-            }
-        </div>
-    );
+
+    if (quotes.length < 1) {
+        return (
+            <div className="quoteWidth">
+                <h4>Quotes</h4>
+                <hr />
+                <p>No quotes!</p>
+            </div>
+        );
+    }
+    else {
+        return(
+            <div className="quoteWidth">
+                <h4>Quotes</h4>
+                <hr />
+                {
+                    quotes.map((quote, index) => {
+                        return (
+                            <Quote key={quote + '-' + index} quote={quote} />
+                        );
+                    })
+                }
+            </div>
+        );
+    }
 }
 
 export default QuoteList;
