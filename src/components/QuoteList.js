5c31d0034cbd59fb232cfe00b32749b7cdf8dc33
Kasperi Pruukka <kasperi.pruukka@gmail.com>
2021-12-16 19:48:52 +0200

    QuoteList-komponentille toiminnallisuutta
    Tuodaan komponentille käyttöön useQuotes-hook.
    Mapataan quotes-muuttujan ilmentymät käyttöliittymään.

--- src/components/QuoteList.js
+++ src/components/QuoteList.js
@@ -1,7 +1,19 @@
+import useQuotes from "../useQuotes";
+import Quote from "./Quote";
+
 const QuoteList = () => {
+    const {quotes} = useQuotes();
     return(
-        <div>
-            <h3>Quotes</h3>
+        <div className="quoteWidth">
+            <h4>Quotes</h4>
+            <hr />
+            {
+                quotes.map((quote, index) => {
+                    return (
+                        <Quote key={quote + '-' + index} quote={quote} />
+                    );
+                })
+            }
         </div>
     );
 }
