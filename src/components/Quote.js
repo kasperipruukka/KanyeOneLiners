439f46bd25de127313f57a40be6f84d638bb8861
Kasperi Pruukka <kasperi.pruukka@gmail.com>
2021-12-16 22:10:39 +0200

    Korjataan vielä pois deleteClick-metodin kutsu.

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -10,7 +10,7 @@ const Quote = ({quote}) => {
     return(
         <div className="singleQuote m-3 p-1 jumbotron jumbotron-fluid bg-gradient-to-r from-white via-sky-100 to-white hover:cursor-pointer">
             <div className="container flex justify-center">
-                <p onClick={deleteClick} className="text-center w-3/5 text-lg"><i>{quote}</i></p>
+                <p className="text-center w-3/5 text-lg"><i>{quote}</i></p>
             </div>
         </div>
     );
