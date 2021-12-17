85420e8d4c1430f8a6ac096e03339810a247dbbe
Kasperi Pruukka <kasperi.pruukka@gmail.com>
2021-12-17 19:10:00 +0200

    Siirretään Quoten poistaminen diviin.

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -8,9 +8,9 @@ const Quote = ({id, value}) => {
     }
 
     return(
-        <div className="singleQuote m-3 p-1 jumbotron jumbotron-fluid bg-gradient-to-r from-white via-sky-100 to-white hover:cursor-pointer">
+        <div onClick={deleteClick} className="singleQuote m-3 p-1 jumbotron jumbotron-fluid bg-gradient-to-r from-white via-sky-100 to-white hover:cursor-pointer">
             <div className="container flex justify-center">
-                <p onClick={deleteClick} className="text-center w-3/5 text-lg"><i>{value}</i></p>
+                <p className="text-center w-3/5 text-lg"><i>{value}</i></p>
             </div>
         </div>
     );
